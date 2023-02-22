0766d74f2b07a95bbeb22e87a3feeaf38f28788c
sabriw88 <sabrina.wf@gmail.com>
2023-02-22 18:12:15 -0300

    modificacion app.js y package.json

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const port = process.env.PORT || 3001;
+
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.listen(3000, () => {console.log("Servidor corriendo")});
+/* app.listen(3000, () => {console.log("Servidor corriendo")}); */
+app.listen(port, () => console.log('Servidor corriendo en el puerto ${port}'));
 
 app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname, './views/home.html'))});
 app.get('/register', (req, res) => {res.sendFile(path.resolve(__dirname, './views/register.html'))});
