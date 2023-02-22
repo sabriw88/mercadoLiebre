ddad8bbc12e43fb8445ea3d730fc1879bd5faa8d
sabriw88 <sabrina.wf@gmail.com>
2023-02-22 10:55:03 -0300

    subo proyecto Mercado Liebre

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.listen(3000, () => {console.log("Servidor corriendo")});
+
+app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname, './views/home.html'))});
+app.get('/register', (req, res) => {res.sendFile(path.resolve(__dirname, './views/register.html'))});
+app.get('/login', (req, res) => {res.sendFile(path.resolve(__dirname, './views/login.html'))});
+app.get('/add-product', (req, res) => {res.sendFile(path.resolve(__dirname, './views/add-product.html'))});
